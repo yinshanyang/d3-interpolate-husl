8bf10685b8d343de40f52e21f31c39c7ce09cdf6
Yin Shanyang <yang@swarm.is>
2015-06-18 18:12:13 +0800

    build system and direct-to-browser(tm) scripts

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-export { default as interpolateHusl } from './husl';
-export { default as interpolateHuslp } from './huslp';
+import interpolateHusl from './husl';
+import interpolateHuslp from './huslp';
+
+if (typeof window !== 'undefined') {
+  if (window.d3 !== undefined) {
+    window.d3.interpolateHusl = interpolateHusl;
+    window.d3.interpolateHuslp = interpolateHuslp;
+  }
+}    
+
+export { interpolateHusl as interpolateHusl };
+export { interpolateHuslp as interpolateHuslp };
